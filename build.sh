4c4ffd80c9a478bc8f72e1b8f6cae906cb17ac8e
JP Simard <jp@jpsim.com>
2015-05-06 14:17:29 -0700

    Added iOS Swift CocoaPods example

--- build.sh
+++ build.sh
@@ -5,7 +5,12 @@ cat <<EOF
 Usage: sh $0 command [argument]
 
 command:
-  bootstrap: downloads product dependencies and runs `pod install` where appropriate
+  bootstrap:                downloads product dependencies and runs `pod install` where appropriate
+  test-all:                 tests all projects in this repo.
+  test-ios-objc-static:     tests iOS Objective-C static example.
+  test-ios-objc-cocoapods:  tests iOS Objective-C CocoaPods example.
+  test-ios-swift-dynamic:   tests iOS Swift dynamic example.
+  test-ios-swift-cocoapods: tests iOS Swift CocoaPods example.
 EOF
 }
 
@@ -39,6 +44,10 @@ case "$COMMAND" in
             cd ios/objc/CocoaPodsExample
             pod install
         )
+        (
+            cd ios/swift/CocoaPodsExample
+            pod install
+        )
 
         # Remove downloaded zips
         rm -rf *.zip
@@ -53,6 +62,7 @@ case "$COMMAND" in
         ./build.sh test-ios-objc-static
         ./build.sh test-ios-objc-cocoapods
         ./build.sh test-ios-swift-dynamic
+        ./build.sh test-ios-swift-cocoapods
         exit 0
         ;;
 
@@ -68,6 +78,11 @@ case "$COMMAND" in
 
     "test-ios-swift-dynamic")
         xcodebuild -project ios/swift/DynamicExample/DynamicExample.xcodeproj -scheme DynamicExample clean build test -sdk iphonesimulator
+        exit 0
+        ;;
+
+    "test-ios-swift-cocoapods")
+        xcodebuild -workspace ios/swift/CocoaPodsExample/CocoaPodsExample.xcworkspace -scheme CocoaPodsExample clean build test -sdk iphonesimulator
         exit 0
         ;;
 
